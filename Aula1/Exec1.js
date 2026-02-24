9644e488fb59b0ed55b5593082851a5b75b3d2e0
gbsalermo <enzogabrielskull@gmail.com>
2026-02-24 11:15:41 -0300

    Console.log e primeiro exercicio

---
+++ Aula1/Exec1.js
@@ -0,0 +1,3 @@
+//Exibir um texto dizendo: Meu nome é "NOME". estou aprendendo JavaScript às 10 da manhã.
+
+console.log('Meu nome é "Gabriel salermo". Estou aprendendo JavaScript às', 10, "da manhã");
