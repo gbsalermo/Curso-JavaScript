e89d1f36bb75e75c87e7781b41e7a9e8d0a27b65
gbsalermo <enzogabrielskull@gmail.com>
2026-04-22 22:36:11 -0300

    Conclusão de exercicio do NODE.JS

--- SESSAO-0010/AULA 7 - EXERCICIO NodeList/assets/js/main.js
+++ SESSAO-0010/AULA 7 - EXERCICIO NodeList/assets/js/main.js
@@ -1,1 +1,39 @@
-alert(1);
+//O exercicio pede duas coisas: PRiMEIRO COLOCAR A COR DE backgroundColorBody NO FUNDO DOS PARAGRAFOS E TAMBEM COLOCAR A COR DO TEXTO DO PARAGRAFO EM BRANCO
+
+//QuerySelector retorna o primeiro elemento que tem a classe, no caso abaixo paragrafos
+/* const paragrafos = document.querySelector('.paragrafos');
+const ps = paragrafos.querySelectorAll('.p'); // pegando paragrafos de dentro de uma div
+
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+//console.log(backgroundColorBody);
+
+//interando nos elementos
+for (let p of ps){
+    ps.style.backgroundColorBody = backgroundColorBody;
+} */
+
+
+
+//function copiarBackground(){
+
+    //Seleciono a classe da div
+    const paragrafos = document.querySelector('.container');
+    
+    //Seleciono a cor, que nesse caso vai ser a do body
+    const sty = window.getComputedStyle(document.body);
+    const background = sty.backgroundColor;
+
+    //Seleciono todos os paragrafos
+    const ps = document.querySelectorAll('.ps')
+
+    //Uso o laço para alterar todos os valores
+    for (let p of ps){
+        //Esse backgroundColor remete ao css no metodo background-Color
+        p.style.backgroundColor = background; //mudando o background para a cor do body
+        p.style.color = "White"; //mudar apenas a letra para branco
+    }
+
+
+//}
